58093f3aa08085e85aa403d62efe7a62a87eca63
Suchea <bsuchea@gmail.com>
2022-08-31 22:24:52 +0700

    jquery and send data ajax on purchase

--- purchase.php
+++ purchase.php
@@ -77,42 +77,47 @@ require_once 'inc/html_head.php';
                         <form action="#" method="post">
                             <div class="form-row">
                                 <div class="form-group col-md-6">
+                                    
                                     <label for="inputID">Supplier Name</label>
-                                    <select name="suppliers_name" class="form-select">
+                                    <select name="suppliers_name" id="suppliers_name" class="form-select">
                                         <option hidden value="">Select Supplier</option>
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
+                                        <?php 
+                                            $stmt = $con->query("SELECT * FROM tbl_suppliers");
+                                            while($row = $stmt->fetch(PDO::FETCH_OBJ)):
+                                        ?>
+                                        <option value="<?= $row->suppliers_id ?>"><?= $row->suppliers_name ?></option>
+                                        <?php endwhile; ?>
                                     </select>
                                 </div>
                                 <div class="form-group col-md-6">
                                     <label for="form-select" class="form-label">Purchase Date</label>
-                                    <input type="date" name="purchase_date" class="form-control" placeholder="Quantity">
+                                    <input type="date" name="purchase_date" id="purchase_date" class="form-control" placeholder="Quantity">
                                 </div>
                             </div>
                                 <div class="form-row">
                                 <div class="form-group col-md-4">
                                     <label for="inputID">Product Name</label>
-                                    <select name="item_name" class="form-select">
+                                    <select name="item_name" id="item_name" class="form-select">
                                         <option hidden value="">Select Product</option>
-                                        <option value="1">1</option>
-                                        <option value="2">2</option>
-                                        <option value="3">3</option>
-                                        <option value="4">4</option>
+                                        <?php 
+                                            $stmt = $con->query("SELECT * FROM tbl_item");
+                                            while($row = $stmt->fetch(PDO::FETCH_OBJ)):
+                                        ?>
+                                        <option value="<?= $row->item_id ?>"><?= $row->item_name ?></option>
+                                        <?php endwhile; ?>
                                     </select>
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="form-select" class="form-label">Quantity</label>
-                                    <input type="number" name="purchase_qty" class="form-control" placeholder="Quantity">
+                                    <input type="number" name="purchase_qty" id="purchase_qty" min="1" class="form-control" placeholder="Quantity">
                                 </div>
                                 <div class="form-group col-md-4">
                                     <label for="form-select" class="form-label">Unit Price ($)</label>
-                                    <input type="text" name="purchase_unit_price" class="form-control" placeholder="Unit Price">
+                                    <input type="text" name="purchase_unit_price" id="purchase_unit_price" min="1" class="form-control" placeholder="Unit Price">
                                 </div>
                             </div>
                             <div class="text-center mt-3">
-                                <button name="#" type="submit" class="btn btn-sm btn-info rounded-5">
+                                <button name="#" type="submit" id="btnAdd" class="btn btn-sm btn-info rounded-5">
                                 <svg width="16" height="16" fill="white" class="bi bi-plus-circle" viewBox="0 0 16 16">
                                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                 <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
@@ -128,10 +133,9 @@ require_once 'inc/html_head.php';
                         <div class="app-card app-card-orders-table shadow-sm mb-5 mt-1">
                             <div class="app-card-body">
                                 <div class="table-responsive">
-                                    <table class="table app-table-hover mb-0 text-left">
+                                    <table id="tblproduct" class="table app-table-hover mb-0 text-left">
                                         <thead>
                                             <tr>
-                                                <th class="cell text-center">#</th>
                                                 <th class="cell">Product Name</th>
                                                 <th class="cell">Unit Price ($)</th>
                                                 <th class="cell">Quantity</th>
@@ -152,11 +156,11 @@ require_once 'inc/html_head.php';
                     </div><!--end table-->
                     <!-- Total -->
                     <div style="width: 20%;" class="container bg-white p-3 shadow rounded-4 mr-5">
-                        <h6>Purchase Total ($): </h6>
+                        <h6>Purchase Total ($): <span id="total"></span></h6>
                     </div>
                     <!-- End Total -->
                     <div class="text-center mt-3">
-                                <button name="#" type="submit" class="btn btn-md btn-success rounded-5">
+                                <button name="#" id="btnPurchase" type="submit" class="btn btn-md btn-success rounded-5">
                                     <svg width="16" height="16" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
                                     <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                                     <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
@@ -175,6 +179,70 @@ require_once 'inc/html_head.php';
 
 <!-- ::: My Script ::: -->
 <?php include('contents/My_Scripts.php') ?>
+
+<script>
+    let items = new Array();
+    let total = 0;
+
+    $(document).ready(function(){
+        $("#purchase_date").val(new Date().toISOString().slice(0, 10));
+    });
+
+    $("#btnAdd").click(function(){
+        if($('#item_name').val() == "") {errmsg('Please select item!'); return false;}
+        if($('#purchase_qty').val() == "") {errmsg('Please insert purchase quantity!'); return false; }
+        if($('#purchase_unit_price').val() == "") {errmsg('Please insert purchase unit price!'); return false;}
+
+        let newRowContent = "<td>"+$('#item_name option:selected').text() +"</td>" + "<td>"+$('#purchase_qty').val() +"</td>" + "<td>"+$('#purchase_unit_price').val() +"</td>" + "<td>"+$('#purchase_qty').val() * $('#purchase_unit_price').val() +"</td>";
+
+        $("#tblproduct tbody").append("<tr>"+newRowContent+"</tr>");
+
+        total += $('#purchase_qty').val() * $('#purchase_unit_price').val();
+
+        item = {id:$('#item_name').val(), pqty:$('#purchase_qty').val(), punit:$('#purchase_unit_price').val()};
+
+        items.push(item);
+
+        $("#total").text(total);
+
+        console.log('working!' + JSON.stringify(items));
+        return false;
+    });
+        
+    $("#btnPurchase").click(function(){
+        em = $("#suppliers_name").val();
+        date = $("#purchase_date").val();
+        $.ajax({
+            // url: 'ajax/purchase.php?em='+em+'+date='+date,
+            url: 'ajax/purchase.php',
+            cache: false,
+            data: {items: items, em: em, d: date},
+            success: function(res){
+                if(res == 'success'){
+                    // success
+                    $("#suppliers_name").val('');
+                    $("#purchase_date").val(new Date().toISOString().slice(0, 10));
+                    $('#item_name').val('');
+                    $('#purchase_qty').val('');
+                    $('#purchase_unit_price').val('');
+                    $("#total").text("");
+                    items = new Array();
+                    total = 0;
+                }
+                Swal.fire(res, '', 'success');
+            }, error: function(e){
+                console.log(e.responseText); 
+            }
+        });
+        console.log('working!');
+        return false;
+    });
+
+    function errmsg(msg){
+        Swal.fire(msg, '', 'error'); 
+    }
+</script>
+
 </body>
 </html>
 
