8dd3f39723a4e469c9e6ad200e220c7ccb44b1d1
Ses Sovankiri <99698891+no-oneperfect@users.noreply.github.com>
2022-10-03 08:23:04 +0700

    updated

--- brand_and_category.php
+++ brand_and_category.php
@@ -29,29 +29,23 @@ require_once 'inc/html_head.php';
                         <h1 style="font-family: 'Kantumruy Pro ExtraLight'; color:#15a362; font-size:large"
                             class="app-page-title mb-2">ទំព័រប្រេន និងប្រភេទទំនិញ</h1>
                     </div>
-                    <div class="container bg-white p-3 mt-3 shadow p-3 mb-5 bg-body rounded-5 border border-success">
+                    <div class="container bg-white p-3 mt-3 shadow p-3 mb-5 bg-body rounded-5">
                     <div class="row text-center">
-                        <div style="background-color:#15a362 ;" class="col border rounded-5 ml-2 p-2">
-                        <div>
-                                    <a href="brand.php">
-                                    <svg width="20" height="20" fill="white" class="bi bi-apple" viewB="0 0 16 16">
+                        <div class="col-md-6 rounded-5 p-2 ">
+                            <button type="button"​ style="width: 100%; height:100%; text-decoration:none" class="btn btn-outline-info rounded-5"><a href="brand.php">
+                                    <svg width="20" height="20" fill="black" class="bi bi-apple" viewB="0 0 16 16">
                                     <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z"/>
                                     <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z"/>
                                     </svg>
-                                    </a>
+                            <p>ប្រេនទំនិញ</p></a></button>
                         </div>
-                            <a style="text-decoration: none; color:white;" href="brand.php">ប្រេនទំនិញ</a>
-                        </div>
-                        <div style="background-color:#15a362 ;" class="col border rounded-5 ml-2 mr-2 p-2">
-                        <div>       
-                                    <a href="category.php">
-                                    <svg width="20" height="20" fill="white" class="bi bi-tags" viewB="0 0 16 16">
+                        <div class="col-md-6 rounded-5 p-2 ">
+                            <button type="button"​ style="width: 100%; height:100%; text-decoration:none" class="btn btn-outline-info rounded-5"><a href="category.php">
+                                    <svg width="20" height="20" fill="black" class="bi bi-tags" viewB="0 0 16 16">
                                     <path d="M3 2v4.586l7 7L14.586 9l-7-7H3zM2 2a1 1 0 0 1 1-1h4.586a1 1 0 0 1 .707.293l7 7a1 1 0 0 1 0 1.414l-4.586 4.586a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 2 6.586V2z"/>
                                     <path d="M5.5 5a.5.5 0 1 1 0-1 .5.5 0 0 1 0 1zm0 1a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3zM1 7.086a1 1 0 0 0 .293.707L8.75 15.25l-.043.043a1 1 0 0 1-1.414 0l-7-7A1 1 0 0 1 0 7.586V3a1 1 0 0 1 1-1v5.086z"/>
                                     </svg>
-                                    </a>
-                        </div>
-                            <a style="text-decoration: none; color:white;" href="category.php">ប្រភេទទំនិញ</a>
+                            <p>ប្រភេទទំនិញ</p></a></button>
                         </div>
                     </div>
                     </div>
